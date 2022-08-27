a5fc1536fc217c0f8830d72d9e09df9008714a84
MUKESHKUMAR96084 <112257296+MUKESHKUMAR96084@users.noreply.github.com>
2022-08-27 19:32:21 +0530

    Add files via upload

---
+++ Loop Control/Break Statement/Break.js
@@ -0,0 +1,10 @@
+var x = 0;
+while (x < 20)
+{
+ if (x == 5)
+ { 
+ break;
+ }
+ x = x + 1;
+ console.log(x);
+}
---
+++ Loop Control/Continue Statement/Continue.js
@@ -0,0 +1,11 @@
+var x = 0;
+while (x < 20)
+{
+    x = x + 1;
+ if (x == 5)
+ { 
+ continue; 
+ }
+ 
+ console.log(x);
+}
